8e7b629f7836b5d3cc50263877156effd2412a98
Sanjay <sanjusanjay06406@gmail.com>
2026-07-17 23:23:27 +0530

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDatabase();
 const PORT = process.env.PORT;
 
-const allowOrigins = ["http://localhost:5173","http://localhost:5174"]
+const allowOrigins = ["http://localhost:5173","http://localhost:5174","http://localhost:5175"]
 
 app.use(express.json());
 app.use(cookieParser());
